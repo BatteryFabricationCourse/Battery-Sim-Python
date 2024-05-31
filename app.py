6d96e1b2bcc93bc6ea6f1b67d02bcd4e00adaa6a
Yumish Niroula <yniroula23@univ.yo.oulu.fi>
2024-05-31 17:56:26 +0300

    Lowered to 20 cycles

--- app.py
+++ app.py
@@ -102,37 +102,37 @@ def simulate():
         "Rest for 5 minutes",
     )
     ]
-    * 100
-    + [
-    (
-        "Charge at 1 C until 4.0 V",
-        "Hold at 4.0 V until C/20",
-        "Rest for 30 minutes",
-        "Discharge at C/3 until 2.2 V",
-        "Rest for 30 minutes",
-    ),
-    (
-        "Charge at 1 C until 4.0 V",
-        "Hold at 4.0 V until C/20",
-        "Rest for 30 minutes",
-        "Discharge at 1 C until 2.2 V",
-        "Rest for 30 minutes",
-    ),
-    (
-        "Charge at 1 C until 4.0 V",
-        "Hold at 4.0 V until C/20",
-        "Rest for 30 minutes",
-        "Discharge at 2 C until 2.2 V",
-        "Rest for 30 minutes",
-    ),
-    (
-        "Charge at 1 C until 4.0 V",
-        "Hold at 4.0 V until C/20",
-        "Rest for 30 minutes",
-        pybamm.step.string("Discharge at 3 C until 2.2 V", period=10),
-        "Rest for 30 minutes",
-    ),
-]
+    * 20
+#    + [
+#    (
+#        "Charge at 1 C until 4.0 V",
+#        "Hold at 4.0 V until C/20",
+#        "Rest for 30 minutes",
+#        "Discharge at C/3 until 2.2 V",
+#        "Rest for 30 minutes",
+#    ),
+#    (
+#        "Charge at 1 C until 4.0 V",
+#        "Hold at 4.0 V until C/20",
+#        "Rest for 30 minutes",
+#        "Discharge at 1 C until 2.2 V",
+#        "Rest for 30 minutes",
+#    ),
+#    (
+#        "Charge at 1 C until 4.0 V",
+#        "Hold at 4.0 V until C/20",
+#        "Rest for 30 minutes",
+#        "Discharge at 2 C until 2.2 V",
+#        "Rest for 30 minutes",
+#    ),
+#    (
+#        "Charge at 1 C until 4.0 V",
+#        "Hold at 4.0 V until C/20",
+#        "Rest for 30 minutes",
+#        pybamm.step.string("Discharge at 3 C until 2.2 V", period=10),
+#        "Rest for 30 minutes",
+#    ),
+#]
     )
     sim = pybamm.Simulation(model, parameter_values=parameters, experiment=experiment3)
     print(f"Running simulation Cycling\n")
