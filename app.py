75ff6a75b8b9288fd2bce1fae63deceea249abc2
Yumish Niroula <yniroula23@univ.yo.oulu.fi>
2024-06-05 10:47:01 +0300

    Switch to Vercel Server

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def simulate():
             if PosElectrodeThickness != 0:
                 parameters.update({"Positive electrode thickness [m]": PosElectrodeThickness})
         if cycles is None or cycles == 0:
-            cycles = 2
+            cycles = 1
         
         
         fast_solver = pybamm.CasadiSolver(mode="safe without grid")
@@ -110,36 +110,7 @@ def simulate():
         )
         ]
         * cycles
-    #    + [
-    #    (
-    #        "Charge at 1 C until 4.0 V",
-    #        "Hold at 4.0 V until C/20",
-    #        "Rest for 30 minutes",
-    #        "Discharge at C/3 until 2.2 V",
-    #        "Rest for 30 minutes",
-    #    ),
-    #    (
-    #        "Charge at 1 C until 4.0 V",
-    #        "Hold at 4.0 V until C/20",
-    #        "Rest for 30 minutes",
-    #        "Discharge at 1 C until 2.2 V",
-    #        "Rest for 30 minutes",
-    #    ),
-    #    (
-    #        "Charge at 1 C until 4.0 V",
-    #        "Hold at 4.0 V until C/20",
-    #        "Rest for 30 minutes",
-    #        "Discharge at 2 C until 2.2 V",
-    #        "Rest for 30 minutes",
-    #    ),
-    #    (
-    #        "Charge at 1 C until 4.0 V",
-    #        "Hold at 4.0 V until C/20",
-    #        "Rest for 30 minutes",
-    #        pybamm.step.string("Discharge at 3 C until 2.2 V", period=10),
-    #        "Rest for 30 minutes",
-    #    ),
-    #]
+    
         )
         sim = pybamm.Simulation(model, parameter_values=parameters, experiment=experiment3)
         print(f"Running simulation Cycling\n")
@@ -166,9 +137,6 @@ def simulate():
     except Exception as e:
         err_json = jsonify(["ERROR: " + str(e)])
         return err_json
-
-if __name__ == '__main__':
-    app.run()
     
 def average_array(a):
     averaged_array = []
